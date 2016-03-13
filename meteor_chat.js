daa593d7a72c71e11087d8735a4b3e047055a665
speedyx <admin@invalid.com>
2016-03-13 21:34:10 +0100

    Cambiamos hello por chat y funciona

--- meteor_chat.js
+++ meteor_chat.js
@@ -2,13 +2,13 @@ if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
+  Template.chat.helpers({
     counter: function () {
       return Session.get('counter');
     }
   });
 
-  Template.hello.events({
+  Template.chat.events({
     'click button': function () {
       // increment the counter when button is clicked
       Session.set('counter', Session.get('counter') + 1);
