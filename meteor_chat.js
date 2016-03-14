61cbe70d3e59a43de6aafe269c97d5ec3402b3aa
speedyx <admin@invalid.com>
2016-03-14 07:10:48 +0100

    Agregamos MongoDB coleccion, datos, ej de uso

--- meteor_chat.js
+++ meteor_chat.js
@@ -1,3 +1,6 @@
+// Iniciamos una BBDD: persons
+Persons = new Mongo.Collection('persons');
+
 if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
@@ -52,3 +55,62 @@ if (Meteor.isServer) {
     // code to run on server at startup
   });
 }
+
+//Vamos a colocar objetos en la BBDD
+var personId = Persons.insert({
+  name: 'Tomas Hromnik',
+  nick: 'Bender',
+  age: 29
+});
+
+Persons.insert({
+  name: 'Josh Bender',
+  nick: 'Elfoslav',
+  age: 25
+});
+
+Persons.insert({
+  name: 'Graham Agassiz',
+  nick: 'Aggy',
+  age: 32
+});
+
+Persons.update(personId, { $set: { age: 26 } });
+Persons.update(
+  { name: 'Tomas Hromnik' },
+  { $inc: { age: 1 } }, //Incrementa la edad en 1
+  { multi: true } //Actualiza múltiples documentos
+);
+
+Persons.findOne(personId);
+Persons.findOne({nick: 'Elfoslav'});
+
+Persons.findOne({nick: 'Elfoslav'}, {
+  fields: {
+    name: 1,
+    nick: 1
+  }
+});
+
+Persons.findOne({nick: 'Elfoslav'}, {
+  fields: {
+    nameage: 0
+  }
+});
+
+Persons.find(); //Devuelve el puntero del objeto
+Persons.find().fetch(); //Devuelve un Array de documentos
+
+Persons.find({ name: 'Tomas Hromnik'}).fetch();
+
+Persons.find({}, {
+  sort: {name: 1}, // Ordenado ASC por nombre
+  limit:2
+}).fetch();
+
+Persons.find({}, {
+  sort: {name: -1}, // Ordenado DESC por nombre
+  limit:2
+}).fetch();
+
+Persons.remove({ nick: 'Elfoslav' }); //Borrar Elfoslav de la coleccion Persons
